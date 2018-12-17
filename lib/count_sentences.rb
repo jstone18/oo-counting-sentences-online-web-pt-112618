011ee2cd104d451fe950c6f30e13494323098fb6
Jason Stone <jason.stone0808@gmail.com>
2018-12-17 20:41:23 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,8 @@ class String
   end
 
   def count_sentences
-    self.split(/[.?!]+/).grep(/\S/).count
+    self.split([.?!]+).grep(\S).count
+    # self.split(/[.?!]+/).grep(/\S/).count
     # self.split(/[.?!]+/).count
     # self.split.length
   end
